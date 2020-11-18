bc6a61b46cff839887686eac81ac16d2ca684182
jeasusav10 <je.anayav98@gmail.com>
2020-11-18 13:52:11 -0600

    Performing flask

--- ML_Model/model_Web.py
+++ ML_Model/model_Web.py
@@ -5,12 +5,13 @@ import joblib
 
 app= flask.Flask(__name__)
 
-@app.route('/prueba/<dato>',methods=['POST'])
-def ejemplo_servicio(dato):
-    contenido=flask.request.json
-    print(contenido)
-    ejemplo=np.array([2.67716444, 0.66994484, 1.67683077, -0.13364358, -0.70614255,  1.8910083, 0.75438593, -1.45490372])
-    modelo = joblib.load('model_dt.joblib')
-    respuesta = modelo.predict(ejemplo.reshape(1,-1))
-    print(respuesta)
-    return flask.jsonify({'resultad':respuesta[0]})
+@app.route('/<dato>',methods=['POST'])
+def Home(dato):
+    cont=flask.request.json
+    print(cont)
+    test = np.array([2.67716444, 0.66994484, 1.67683077, -0.13364358, -0.70614255,  1.8910083, 0.75438593, -1.45490372])
+    model = joblib.load('model_dt.joblib')
+    ans = (model.predict(test.reshape(1,-1)))[0]
+    int_ans= int(ans)
+    print(int_ans)
+    return flask.jsonify({'Prediction':int_ans})
